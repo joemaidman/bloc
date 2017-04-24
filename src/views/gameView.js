69a386f96ec09e7b186170e1ca14ca7051856ef4
Ryan <Ryanchu.ws@gmail.com>
2017-04-24 15:45:56 +0100

    adds get canvas method to gameview

--- src/views/gameView.js
+++ src/views/gameView.js
@@ -1,7 +1,15 @@
+"strict mode";
+
 var Canvas = require('canvas');
 
 function GameView(){
-
+  this.canvas = new Canvas(200,200);
 }
+
+GameView.prototype = {
+  getCanvas: function(){
+    return this.canvas;
+  }
+};
 
 module.exports = GameView;
--- test/views/gameView-test.js
+++ test/views/gameView-test.js
@@ -5,9 +5,16 @@ var GameView = require('../../src/views/gameView.js');
 describe("GameView", function(){
 var gameView;
 
+beforeEach(function(){
+  gameView = new GameView();
+});
+
  it("exists", function(){
-   gameView = new GameView();
    expect(gameView).not.to.be.undefined;
   });
 
+it("can return a canvas", function(){
+  console.log(gameView.getCanvas());
+  expect(gameView.getCanvas()).to.be.a('Canvas');
+  });
 });
