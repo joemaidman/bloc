a6d582c32f444bd3c9c86615ab7c84a78fab67e8
Ryan <Ryanchu.ws@gmail.com>
2017-04-26 13:11:19 +0100

    grid lines defined

--- public/client.js
+++ public/client.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
+
   var socket = io.connect();
   var iso = new Isomer(document.getElementById("canvas"));
 
@@ -8,22 +9,28 @@ document.addEventListener("DOMContentLoaded", function(){
   socket.emit('add_block', {block: [0,3,0]});
   socket.emit('add_block', {block: [3,3,0]});
 
-
-  socket.on('updateWorld', function (data) {
-
-  function GridLines () {
-    for (x = 0; x < 13; x++) {
+  function GridLines (xsize, ysize, zheight) {
+    for (x = 0; x < xsize+1; x++) {
       iso.add(new Isomer.Path([
-        new Isomer.Point(x, 10, 0),
-        new Isomer.Point(x, 5, 0),
-        new Isomer.Point(x, 0, 0),
+        new Isomer.Point(x, 0, zheight),
+        new Isomer.Point(x, xsize, zheight),
+        new Isomer.Point(x, 0, zheight),
       ]),
     new Isomer.Color(255, 0, 0));
     }
+    for (y = 0; y < ysize+1; y++) {
+      iso.add(new Isomer.Path([
+        new Isomer.Point(0, y, zheight),
+        new Isomer.Point(ysize, y, zheight),
+        new Isomer.Point(0, y, zheight),
+      ]),
+    new Isomer.Color(255,0,0));
+    }
   }
 
-  GridLines()
+  socket.on('updateWorld', function (data) {
 
+  GridLines(11,11,0);
 
    var blocks = data.blocks;
     for (var i = 0; i<blocks.length; i++ ){
