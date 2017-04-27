5ba0322d93d20096db674030321b853420545d93
joemaidman <joemaidman@gmail.com>
2017-04-27 18:20:47 +0100

    Adds crazy formula to client

--- public/client.js
+++ public/client.js
@@ -3,11 +3,12 @@
 document.addEventListener("DOMContentLoaded", function(){
 
   var socket = io.connect();
-  var iso = new Isomer(document.getElementById("canvas"), { scale: 30 });
+  var iso = new Isomer(document.getElementById("canvas"), { scale: 30});
   var Shape = Isomer.Shape;
   var Point = Isomer.Point;
   var Color = Isomer.Color;
   var Path = Isomer.Path;
+  // var canvas = document.getElementById("canvas");
 
   drawGridLines(11,11,0);
   drawOrigin();
@@ -15,6 +16,41 @@ document.addEventListener("DOMContentLoaded", function(){
   $("#rotate").click(function() {
     socket.emit('rotate');
   });
+
+  function calculate(mouseX, mouseY){
+  var part1 = (mouseX - 300) / 26;
+  var part2 = (((mouseX - 300) / 26) + ((mouseY - 540)/ 15)) / -2;
+  var x = Math.floor(part1 + part2);
+
+  var y = Math.floor((((mouseX - 300) / 26) + ((mouseY - 540) / 15)) / -2);
+
+  console.log("X: " + x + " || Y: " + y);
+  }
+
+
+  function writeMessage(canvas, message) {
+          var context = canvas.getContext('2d');
+          context.clearRect(0, 0, 500, 100);
+          context.font = '12pt Calibri';
+          context.fillStyle = 'black';
+          context.fillText(message, 10, 25);
+        }
+        function getMousePos(canvas, evt) {
+          var rect = canvas.getBoundingClientRect();
+          return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+          };
+        }
+        var canvas = document.getElementById('canvas');
+        var context = canvas.getContext('2d');
+
+        canvas.addEventListener('mousemove', function(evt) {
+          var mousePos = getMousePos(canvas, evt);
+          var message = 'Mouse position: ' + Math.floor(mousePos.x) + ', ' + Math.floor(mousePos.y);
+          writeMessage(canvas, message);
+          calculate(getMousePos(canvas, evt).x, getMousePos(canvas, evt).y);
+        }, false);
 
   $("#add").click(function() {
     var x = parseInt($("#x").val());
