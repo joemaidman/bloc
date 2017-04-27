525ba4a42b97164fa0eb4c3a2c83861ca7b2a51f
Christos <chris.paraskeva10@gmail.com>
2017-04-27 12:01:32 +0100

    WIP adding delete function

--- src/models/game.js
+++ src/models/game.js
@@ -11,11 +11,24 @@ Game.prototype = {
   addShape: function(shape){
     this.shapes.push(shape);
   },
+  deleteShape: function(coordinates){
+    // console.log(this.shapes)
+    this.shapes = this._findShape(coordinates)
+    // console.log(this.shapes)
+  },
   rotateShapes: function(){
     for(var i = 0; i < this.shapes.length; i++){
       this.shapes[i].rotate();
     }
+  },
+  _findShape: function(coordinates){
+    return this.shapes.find(function(shape){
+      console.log(shape.getPosition().toString())
+      console.log(coordinates.toString())
+
+      // console.log(shape.getPosition().toString() !== coordinates.toString());
+    });
   }
-}
+};
 
 module.exports = Game;
--- test/models/game-test.js
+++ test/models/game-test.js
@@ -2,12 +2,18 @@
 
 var Game = require('../../src/models/game.js');
 
-function ShapeDouble() {
+function ShapeDouble(x=0, y=0, z=0) {
+  this.xPos = x;
+  this.yPos = y;
+  this.zPos = z;
 }
 
 ShapeDouble.prototype = {
   rotate: function(){
     return true;
+  },
+  getPosition: function(){
+    return {x: this.xPos, y: this.yPos, z: this.zPos}
   }
 };
 describe("Game", function(){
@@ -17,7 +23,7 @@ describe("Game", function(){
     game = new Game();
   });
 
-  it(".getBlocks returns an array of shapes", function(){
+  it(".getShapes returns an array of shapes", function(){
     expect(game.getShapes()).to.eql([]);
   });
 
@@ -31,6 +37,18 @@ describe("Game", function(){
     game.rotateShapes();
     expect(rotateShapeSpyOne).to.have.been.calledOnce;
     expect(rotateShapeSpyTwo).to.have.been.calledOnce;
+  })
+
+  it (".deleteShape deletes a shape from the shape array", function(){
+    var shapeDoubleOne = new ShapeDouble(1, 2, 0);
+    var shapeDoubleTwo = new ShapeDouble(4, 1, 0);
+    game.addShape(shapeDoubleOne);
+    game.addShape(shapeDoubleTwo);
+    expect(game.getShapes()).to.eql([shapeDoubleOne, shapeDoubleTwo]);
+    console.log(game.getShapes())
+    game.deleteShape({x: 1, y: 2, z: 0});
+    console.log(game.getShapes())
+    // expect(game.getShapes()).to.eql([shapeDoubleTwo]);
   })
 
 });
