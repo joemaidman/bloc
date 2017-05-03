a08e9b9bf2e9f8d65fbad034f02673b60b2ac510
joemaidman <joemaidman@gmail.com>
2017-05-03 11:28:37 +0100

    Testing Heroku depoly

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
     'urldev' : 'mongodb://localhost/bloc_development',
-    'urlprod' : 'mongodb://ds137090.mlab.com:37090/heroku_3cs148ph'
+    'urlprod' : 'mongodb://blocmaster:password@ds137090.mlab.com:37090/heroku_3cs148ph'
 };
