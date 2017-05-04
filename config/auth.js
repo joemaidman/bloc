8f955ed4df3c21ee71d4e6ba7b116b0876e074e5
joemaidman <joemaidman@gmail.com>
2017-05-04 13:31:26 +0100

    Adds || switch to facebook callback URL to use environment variable on heroku

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1965533283679974', // your App ID
         'clientSecret'  : '33061d07884e27838714886c1e3295fc', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : process.env.FBCALLBACK ||  'http://localhost/auth/facebook/callback'
     }
   };
